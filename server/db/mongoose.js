d3d5e6e17c64b8e369bee6361227e8cc7f091f79
Andres Morales <andmoraho@gmail.com>
2018-09-18 12:57:10 -0500

    Test the DELETE /todos/:id route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-// 'mongodb://localhost:27017/TodoApp'
 // 'mongodb://andmoraho:todoapp123@ds121982.mlab.com:21982/andmorahotodoapp'
 mongoose.connect('mongodb://andmoraho:todoapp123@ds121982.mlab.com:21982/andmorahotodoapp', { useNewUrlParser: true });
 
