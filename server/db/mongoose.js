4b862cf5211e36a12a475449a7dc87ff8082cf76
Andres Morales <andmoraho@gmail.com>
2018-09-20 10:20:06 -0500

    Setup separate test and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,8 +2,9 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 // 'mongodb://localhost:27017/TodoApp'
+// 'mongodb://localhost:27017/TodoAppTest'
 // 'mongodb://andmoraho:todoapp123@ds121982.mlab.com:21982/andmorahotodoapp'
-mongoose.connect('mongodb://andmoraho:todoapp123@ds121982.mlab.com:21982/andmorahotodoapp', { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 
 mongoose.set('useCreateIndex', true);
 mongoose.set('useFindAndModify', false);
