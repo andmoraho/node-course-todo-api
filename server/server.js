43bda08637619c2632a0d49a5022ce4d0d40e4ed
Andres Morales <andmoraho@gmail.com>
2018-09-28 11:05:16 -0500

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -101,7 +101,6 @@ app.patch('/todos/:id', (req, res) => {
 });
 
 //Users
-
 app.post('/users', (req, res) => {
     var body = _.pick(req.body, ['email', 'password']);
     var user = new User(body);
@@ -117,6 +116,18 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then((user) => {
+        return user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).status(200).send(user);
+        });
+    }).catch((error) => {
+        res.status(400).send();
+    });
 });
 
 app.listen(port, () => {
